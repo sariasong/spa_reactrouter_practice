e0104ccc8903f063b3c91d8075a006fb29e591a2
Sarah Daenitz <sarahdaenitz@gmail.com>
2018-06-13 12:06:41 -0600

    protected route added

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import NavBar from './components/NavBar';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import Product from './components/Product';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => (
   <Fragment>
@@ -15,8 +16,8 @@ const App = () => (
         <Route exact path="/" component={Home} />,
         <Route exact path="/about" component={About} />
         <Route exact path="/login" component={Login} />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/products/:id" component={Product} />
+        <ProtectedRoute exact path="/dashboard" component={Dashboard} />
+        <ProtectedRoute exact path="/products/:id" component={Product} />
         <Route component={NoMatch} />
     </Switch>
    </NavBar>
