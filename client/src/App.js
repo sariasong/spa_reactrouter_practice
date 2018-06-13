9c37ad46533dda3e99b829408fbabb337f07a6bb
Sarah Daenitz <sarahdaenitz@gmail.com>
2018-06-13 10:27:27 -0600

    home and about components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
+import About from './components/About';
+import NoMatch from './components/NoMatch';
 
 const App = () => (
-   <Route exact path="/" component={Home} />
+  <Fragment>
+    <Switch>
+      <Route exact path="/" component={Home} />,
+      <Route exact path="/about" component={About} />
+      <Route component={NoMatch} />
+   </Switch>
+  </Fragment>
 );
 
 export default App;
