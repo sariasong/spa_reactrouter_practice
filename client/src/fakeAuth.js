0175e27f90d8ceb9ab4ce85ae6551042942e8b74
Sarah Daenitz <sarahdaenitz@gmail.com>
2018-06-13 11:10:17 -0600

    authentication

---
+++ client/src/fakeAuth.js
@@ -0,0 +1,12 @@
+export const isAuthenticated = () => {
+  return localStorage.isAuthenticated
+}
+
+export const login = () => {
+  localStorage.setItem('isAuthenticated', true)
+}
+
+export const logout = () => {
+  localStorage.removeItem('isAuthenticated'
+  )
+}
